bd7eecb05d0cd0bc0d6f4919fd4d6bd26c486e42
GHOST <ghostdevbusiness@gmail.com>
2022-09-03 23:52:10 +0000

    refactor: use function keyword

--- src/utils/prompts.js
+++ src/utils/prompts.js
@@ -1,11 +1,11 @@
 import prompts from 'prompts';
 import k from 'kleur';
 
-export const onCancel = () => {
+export function onCancel() {
     console.log();
     console.log(`  ${k.bold().red('Exited')} ${k.blue().dim('create-ghost')}`);
     process.exit(0);
-};
+}
 
 export async function checkForce(file, dir = false) {
     const message = `Is it ok to ${k.red('OVERWRITE')} ${
