2cd85cd348e1209e5c6be0f0add81566122daa96
Willow (GHOST) <git@willow.sh>
2025-11-26 23:54:19 +0000

    feat: update worker-simple template

--- files/templates/worker-simple/src/index.ts
+++ files/templates/worker-simple/src/index.ts
@@ -1,6 +1,3 @@
-// biome-ignore lint/complexity/noBannedTypes:
-type Env = {};
-
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext) {},
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {},
